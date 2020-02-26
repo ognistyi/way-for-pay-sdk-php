e312eb700f34e0240497aee4fb762f56c505b11a
ognistyi <ognistyi@gmail.com>
2020-02-26 17:42:37 +0200

    add files

---
+++ src/WayForPay/WayForPayComponent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ognistyi\WayForPay;
+
+use yii\base\BaseObject;
+
+class WayForPayComponent extends BaseObject
+{
+    public $merchantAccount;
+    public $merchantSecretKey;
+}
